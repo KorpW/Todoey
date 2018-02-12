b3af8a7de8376d5d056eb17377bc0f0ab3c4ac19
Magnus Wærstad <magnus@magnuswaerstad-MacBookPro.local>
2018-02-12 23:22:51 +0100

    Convert to MVC design pattern

---
+++ Todoey/Controllers/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Magnus Eri Wærstad on 12.02.2018.
+//  Copyright © 2018 Magnus Eri Wærstad. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
